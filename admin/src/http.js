69261b7d084a298f1f5898700182fb7515efc980
jerkey <lizekai_97@163.com>
2019-09-18 21:36:55 +0800

    王者荣耀项目第一天

---
+++ admin/src/http.js
@@ -0,0 +1,7 @@
+import axios from 'axios'
+
+const http = axios.create({  //创建一个实例
+    baseURL:'http://localhost:3008/admin/api'
+})
+
+export default http
---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+
+const app = express()
+
+app.use(require('cors')())//跨域模块
+app.use(express.json())  //加上中间键
+
+
+
+require('./plugins/db')(app)
+require('./router/admin')(app)
+
+
+app.listen(3008,() => {
+    console.log("http://localhost:3008")
+})
